7d1fb2e8a6209017a26b928aff0007d5581dd387
maevadeleurme <maeva.deleurme@reseau.eseo.fr>
2026-04-03 11:40:12 +0200

    cadeau Raphaël

--- Visuel/PushToDataBase.py
+++ Visuel/PushToDataBase.py
@@ -17,7 +17,7 @@ whiteTexture = [
 with open("DefaultTexture.json","r") as dataBase :
     dicTexture = load(dataBase)
 
-dicTexture["orbTexture"] = orbTexture
+dicTexture["chestTexture"] = chestTexture
 
 with open("DefaultTexture.json","w") as dataBase :
     dump(dicTexture,dataBase,indent = 4)
